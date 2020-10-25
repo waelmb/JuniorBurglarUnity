14c1bd187ccf110899751a51503e055d57ae7aa9
waelmb <wafm2007@gmail.com>
2020-10-25 01:14:39 -0500

    HomeSwitch works

--- JuniorBurglar/Assets/Movement.cs
+++ JuniorBurglar/Assets/Movement.cs
@@ -10,7 +10,8 @@ public class Movement : MonoBehaviour
     public bool isFacingRight = false;
 
     float horizontalInput = 0f;
-    
+    float verticalInput = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,13 +30,27 @@ public class Movement : MonoBehaviour
     void move()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         //Movement
         Vector3 horizontal = new Vector3(horizontalInput, 0.0f, 0.0f);
-        Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+        Vector3 vertical = new Vector3(0.0f, verticalInput, 0.0f);
         //UnityEngine.Debug.Log(horizontal);
         //UnityEngine.Debug.Log(transform.position);
-        transform.position = transform.position + horizontal * RunSpeed * Time.deltaTime;
-        transform.position = transform.position + vertical * RunSpeed * Time.deltaTime;
+        //transform.position = transform.position + horizontal * RunSpeed * Time.deltaTime;
+        //transform.position = transform.position + vertical * RunSpeed * Time.deltaTime;
+
+        if(horizontalInput > 0)
+        {
+            transform.Translate(horizontal * RunSpeed * Time.deltaTime * -1f);
+        }
+        else
+        {
+            transform.Translate(horizontal * RunSpeed * Time.deltaTime);
+        }
+        
+        transform.Translate(vertical * RunSpeed * Time.deltaTime);
+        //GetComponent<Rigidbody2D>().MovePosition(transform.position + position * RunSpeed * Time.deltaTime);
+
     }
 
     void flipSprite()
--- JuniorBurglar/Assets/Scripts/SceneSwitch.cs
+++ JuniorBurglar/Assets/Scripts/SceneSwitch.cs
@@ -7,13 +7,22 @@ public class SceneSwitch : MonoBehaviour
 {
     public string sceneToLoad;
 
-    public void OnTriggerEnter(Collider other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        UnityEngine.Debug.Log("here");
+        UnityEngine.Debug.Log("here onTrigger");
 
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             SceneManager.LoadScene(sceneToLoad);
         }
     }
+
+    /*
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        UnityEngine.Debug.Log("here onCollision");
+        print("here onCollision print");
+    }
+    https://stackoverflow.com/questions/18281385/oncollisionenter-not-working-in-unity3d
+    */
 }
