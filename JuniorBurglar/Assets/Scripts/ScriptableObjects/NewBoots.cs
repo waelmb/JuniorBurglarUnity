6f22067d10431e297b9d402e758c295161a31175
Mike Lazzar <mlazza5@uic.edu>
2020-10-28 00:40:16 -0500

    Scriptable object boots

---
+++ JuniorBurglar/Assets/Scripts/ScriptableObjects/NewBoots.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Boots", menuName = "Boots")]
+public class NewBoots : ScriptableObject
+{
+    public string name;
+    public string description;
+    public int dex;
+    public Sprite artwork;
+}
